42216de25ad832eba31934262c8977de07420dd4
afytahi-exact <73951553+afytahi-exact@users.noreply.github.com>
2020-11-09 12:56:36 -0500

    Update update.ps1

--- update.ps1
+++ update.ps1
@@ -1,1 +1,8 @@
-[string](gcm pwsh -errorAction SilentlyContinue).version | ? {if (!($_ -like (((irm https://raw.githubusercontent.com/PowerShell/PowerShell/master/tools/metadata.json).ReleaseTag -replace '^v') + "*"))) {iex "& { $(irm https://aka.ms/install-powershell.ps1) } -UseMSI -Quiet"}}
+$curVer = [string](gcm pwsh -errorAction SilentlyContinue).version
+$latVer = (irm https://raw.githubusercontent.com/PowerShell/PowerShell/master/tools/metadata.json).ReleaseTag
+
+if (!($curVer -like ($latVer + "*"))) {
+	iex "& { $(irm https://aka.ms/install-powershell.ps1) } -UseMSI -Quiet"
+} else {
+	write-host "Version is already the latest"
+}

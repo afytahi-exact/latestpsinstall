349c2a629dddcb03c5f6779eaebf56a7b108d601
afytahi-exact <73951553+afytahi-exact@users.noreply.github.com>
2020-11-09 13:01:08 -0500

    Update update.ps1

--- update.ps1
+++ update.ps1
@@ -1,6 +1,10 @@
+# Get current version based off pwsh.exe info 
 $curVer = [string](gcm pwsh -errorAction SilentlyContinue).version
+
+# Get latest version based off github repo metadata
 $latVer = (irm https://raw.githubusercontent.com/PowerShell/PowerShell/master/tools/metadata.json).ReleaseTag -replace "v"
 
+# Test versions, if not latest then install latest, else report version is latest
 if (!($curVer -like ($latVer + "*"))) {
 	iex "& { $(irm https://aka.ms/install-powershell.ps1) } -UseMSI -Quiet"
 } else {

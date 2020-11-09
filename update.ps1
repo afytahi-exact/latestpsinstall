70bc40c8c416135a6835edb5f35c018b6705827b
afytahi-exact <73951553+afytahi-exact@users.noreply.github.com>
2020-11-09 13:19:13 -0500

    Update update.ps1

--- update.ps1
+++ update.ps1
@@ -7,6 +7,10 @@ $latVer = (irm https://raw.githubusercontent.com/PowerShell/PowerShell/master/to
 # Test versions, if not latest then install latest, else report version is latest
 if (!($curVer -like ($latVer + "*"))) {
 	iex "& { $(irm https://aka.ms/install-powershell.ps1) } -UseMSI -Quiet"
+	
+	# Restart Syncro Services
+	get-service | ? {$_ -like "*Syncro*"} | restart-service
+	
 } else {
 	write-host "Version is already the latest"
 }
